b3821aa6227c00dc90fccfd97644edb8b23fbd53
Jiah <itisgia@gmail.com>
2018-08-06 11:47:57 +1200

    styling

--- js/script.js
+++ js/script.js
@@ -8,10 +8,69 @@ function initmap() {
 
     var mapOptions = {
         center:{
-            lat: 37.566535,
-            lng: 126.977969
+            lat: 37.515021,
+            lng: 127.088739
         },
-        zoom: 15
+        zoom: 4,
+        disableDefaultUI: false,
+        fullscreenControl: true,
+        fullscreenControlOptions: {
+            position: google.maps.ControlPosition.BOTTOM_CENTER
+        },
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.TOP_CENTER
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.TOP_LEFT
+        },
+        styles : [
+            {
+                featureType: "water",
+                stylers : [
+                    {
+                        color : "#606b61"
+                    }
+                ]
+            },
+            {
+                featureType: "landscape",
+                stylers : [
+                    {
+                        color: "#c4c4c4"
+                    }
+                ]
+
+            },
+            {
+                  featureType: "road.highway",
+                  stylers : [
+                      {
+                          color: "#d6bcbc"
+                      }
+                  ]
+            },
+            {
+                  featureType: 'poi.park',
+                  elementType: 'geometry',
+                  stylers : [
+                      {
+                          color: "#575957"
+                      }
+                  ]
+            },
+            {
+                  featureType: 'poi',
+                  elementType: 'labels.text.fill',
+                  stylers : [
+                      {
+                          color: "#879dc4"
+                      }
+                  ]
+            }
+        ]
     }
 
     var map = new google.maps.Map(document.getElementById('map'), mapOptions)
