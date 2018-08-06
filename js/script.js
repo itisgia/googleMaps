065d7140143a607053da29fd7a9e23313a1c4a4c
Jiah <itisgia@gmail.com>
2018-08-07 09:28:13 +1200

    get json data

--- js/script.js
+++ js/script.js
@@ -3,15 +3,16 @@
 // people have  turned on / off js.
 // if you do it this way, you won'thave an error whatsoever
 google.maps.event.addDomListener(window, 'load', initmap);
+var map; // making in global
 
 function initmap() {
 
     var mapOptions = {
         center:{
-            lat: 37.515021,
-            lng: 127.088739
+            lat: -41.286460,
+            lng: 174.776236
         },
-        zoom: 4,
+        zoom: 12,
         disableDefaultUI: false,
         fullscreenControl: true,
         fullscreenControlOptions: {
@@ -72,6 +73,40 @@ function initmap() {
             }
         ]
     }
+    // my ver
+    // var botanicGarden = {
+    //         lat:  -41.282308,
+    //         lng: 174.767451
+    // }
+    // var map = new google.maps.Map(document.getElementById('map'), mapOptions, {zoom: 4, center: botanicGarden})
+    // var marker = new google.maps.Marker({position: botanicGarden, map: map});
 
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions)
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    addAllMarkrers();
+
+    // marker referance.
+    // var myLatlng = new google.maps.LatLng(-41.286460,174.776236);
+    // var marker = new google.maps.Marker({
+    // position: myLatlng,
+    // title:"Hello World!"
+    // });
+    //
+    // // To add the marker to the map, call setMap();
+    // marker.setMap(map);
+}
+
+// gettubg markers data
+function addAllMarkrers() {
+    $.ajax({
+        url: 'data/mark.json',
+        type: 'GET',
+        dataType: 'json',
+        success: function (markers) {
+            console.log(markers);
+        },
+        error: function (error) {
+            console.log("ERROR, SOMETHING WENT WRONG");
+            console.log(error);
+        }
+    });
 }
